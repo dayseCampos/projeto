6347d2a60aba213dba5473c440650023a64bc9ff
Dayse Campos <111383784+dayseCampos@users.noreply.github.com>
2023-05-24 09:38:42 -0300

    Iniciando projeto

---
+++ script.js
@@ -0,0 +1,37 @@
+//menu sticky
+const headerSticky = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+	headerSticky.classList.toggle ("sticky", window.scrollY >0);
+});
+
+//menu responsivo
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navbar.classList.toggle('open');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navbar.classList.remove('open');
+};
+
+//scroll reveal
+const sr = ScrollReveal ({
+	distance: '25px',
+	duration: 2500,
+	reset: true
+})
+
+sr.reveal('.home-text',{delay:190, origin:'bottom'})
+
+sr.reveal('.sobre,.destaques,.materias,.contato',{delay:200, origin:'bottom'})
